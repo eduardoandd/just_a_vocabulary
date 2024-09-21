6733be67b9682fd89beda4ab2eca9a0f03995161
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-21 16:31:59 -0300

    configurando o admin do model

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from vocabulary.models import Vocabulary
 
-# Register your models here.
+class VocabularyAdmin(admin.ModelAdmin):
+    list_display=('word','translation','total_hits','total_errors')
+    search_fields=('word',)
+
+admin.site.register(Vocabulary,VocabularyAdmin)
