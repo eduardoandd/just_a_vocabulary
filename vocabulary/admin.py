7c0bf728329989a672e6a2feca77386afbc47a15
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-21 18:05:06 -0300

    adicionando fk e migration

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from vocabulary.models import Vocabulary
+from vocabulary.models import Vocabulary,Type
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display=('id','name',)
+    search_fields=('name',)
 
 class VocabularyAdmin(admin.ModelAdmin):
-    list_display=('word','translation','total_hits','total_errors')
+    list_display=('word','translation','type','total_hits','total_errors',)
     search_fields=('word',)
 
+
 admin.site.register(Vocabulary,VocabularyAdmin)
+admin.site.register(Type,TypeAdmin)
+
--- vocabulary/models.py
+++ vocabulary/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
+class Type(models.Model):
+    id=models.AutoField(primary_key=True)
+    name= models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
 class Vocabulary(models.Model):
     id= models.AutoField(primary_key=True)
     word= models.CharField(max_length=100)
     translation= models.CharField(max_length=100)
+    type=models.ForeignKey(Type,on_delete=models.PROTECT, related_name='vocabulary_type')
     total_hits= models.IntegerField()
     total_errors=models.IntegerField()
+    
+    def __str__(self):
+        return self.word
+    
+    
+    
