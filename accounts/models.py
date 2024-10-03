4b4ba86cd454858825e245dddcc2a77c8c537b1d
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-10-03 14:24:24 -0300

    refazendo lógica de login

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Insights(models.Model):
+    id_insights=models.AutoField(primary_key=True)
+    errors_count=models.IntegerField()
+    hit_count=models.IntegerField()
+
+class User(models.Model):
+    id_user=models.AutoField(primary_key=True)
+    username=models.CharField(max_length=30)
+    password=models.CharField(max_length=15)
+    id_insights=models.ForeignKey(Insights,on_delete=models.PROTECT, related_name='user_insights')
+    
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,37 +1,68 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate,login,logout
+from accounts.models import *
+
 
 def register_view(request):
-    if request.method == "POST":
-        user_form=UserCreationForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-            return redirect('login')
     
-    else:
-        user_form=UserCreationForm()
-    
-    return render(request, 'register.html', {'user_form': user_form})
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        
+        user=User()
+        insight=create_insight()
+        
+        user.username=request.POST.get('username')
+        user.password=request.POST.get('password')
+        user.id_insights=insight
+        user.save()
+        
+        request.session['username'] = request.POST.get('username')
+        request.session['password'] = request.POST.get('password')
+        
+        print(request.session['username'])
+        print(request.session['password'])
+        
+        return redirect('login')
 
 def login_view(request):
     
-    if request.method=="POST":
-        username= request.POST['username']
-        password= request.POST['password']
-        user= authenticate(request,username=username, password=password)
+    if request.method == 'GET':
+    
+        username=request.session.get('username')
+        password= request.session.get('password')
+        
+        request.session.pop('username', None)
+        request.session.pop('password', None)
+        
+        return render(request, 'login.html', {'username':username, 'password':password})
+    
+    elif request.method ==  'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username, password=password)
         
         if user is not None:
             login(request,user)
             return redirect('random_form')
         else:
-            login_form=AuthenticationForm()
-    else:
-        login_form= AuthenticationForm()
-    
-    return render(request, 'login.html', {'login_form': login_form})
+            return render(request, 'login.html')
+        
+    return render(request, 'random_form', {'user':user})
+        
+        
+
     
 def logout_view(request):
     logout(request)
     return redirect('login')
-        
+
+
+def create_insight():
+   insight= Insights.objects.create(
+        errors_count=0,
+        hit_count=0
+    )
+   print(type(insight))
+   return insight
